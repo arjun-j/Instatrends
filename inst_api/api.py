559ded943ce2a96dfff1ab879b7cf8cbcc56abb5
root <root@DESKTOP-A60P34V.Home>
2016-08-14 00:04:45 -0500

    Removed random merge giberish

--- inst_api/api.py
+++ inst_api/api.py
@@ -58,11 +58,6 @@ class API:
 		
 
 if __name__ == '__main__':
-<<<<<<< HEAD
 	new_api = API(48.858844, 2.294351, 1000)
 	new_api.run()
-=======
-	new_api = API(35.775445, -78.687043, 1000)
-	new_api.get_data()
->>>>>>> master
 
